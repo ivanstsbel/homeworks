54cb4bc8d69e234639a93e597cbbcc94d150d8c2
vanchBim <stecenkoivan1995@gmail.com>
2022-01-24 18:45:51 +0300

    add revers

---
+++ ConsoleApp2/revers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public static class revers
+    {
+
+        public static string reverse(string str)
+        {
+            int len = str.Length;
+            char[] sReverse = str.ToCharArray();
+            Array.Reverse(sReverse);
+            str = new string(sReverse);
+            return str;
+        }
+    }
+}
